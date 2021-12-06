1f465ba5f5f16c8aaca2879670d344e24dd6386a
St44n <stanislas.roesler@gmail.com>
2021-12-06 22:23:16 +0100

    wzory ABCD

--- Double_Pendulum.py
+++ Double_Pendulum.py
@@ -2,7 +2,7 @@ print('Hello world!')
 
 
 import numpy as np
-n=0
+
 m_1=1
 m_2=1
 L_1=1
@@ -12,19 +12,22 @@ g=10
 
 def f_1(theta_1,theta_2,omega_1,omega_2):
     gamma = theta_1-theta_2
-    return (g(M*np.sin(theta_2)*np.cos(gamma)-np.sin(theta_1))
-     -M*(L_2*((omega_2)^2)+L_1*((omega_1)^2)*np.cos(gamma))*np.sin(gamma))\
-    /(L_1(1-M*(np.cos(gamma))^2))
+    A=M*np.sin(theta_2)*np.cos(gamma)-np.sin(theta_1)
+    B=M*L_2*((omega_2)^2)+L_1*((omega_1)^2)
+    C=np.cos(gamma)*np.sin(gamma)
+    D=L_1*(1-M*(np.cos(gamma))^2)
+    return (g*(A-B)*C)/D
 
 
 def f_2(theta_1, theta_2, omega_1, omega_2):
     gamma = theta_1 - theta_2
+    A=(M*np.sin(theta_1)*np.cos(gamma)-np.sin(theta_2))
+    B=(L_1*((omega_1)^2)+M*L_2*((omega_2)^2)*np.cos(gamma))**np.sin(gamma)
+    C=(L_2*(1-M*(np.cos(gamma))^2))
+    return (g*A+B)/C
 
-    return (g(M*np.sin(theta_1)*np.cos(gamma)-np.sin(theta_2))
-     +(L_1*((omega_1)^2)+M*L_2*((omega_2)^2)*np.cos(gamma))*np.sin(gamma))\
-    /(L_2(1-M*(np.cos(gamma))^2))
-
-
+K_th1_1=0
+K_th2_1=0
 
 
 
