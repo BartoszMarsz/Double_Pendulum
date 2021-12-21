47036f4e6690a7a2ca5091eadf8b5c7b7c6c48d6
St44n <stanislas.roesler@gmail.com>
2021-12-21 12:30:38 +0100

    zakresy osi x

--- plot.py
+++ plot.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+
 data = np.loadtxt('trajectory.pdb', delimiter=';')
 time=data[:,0]
 th1=data[:,1]
 th2=data[:,2]
 om1=data[:,3]
 om2=data[:,4]
+t=data[-1][0]
+
 
 plt.subplot(221)
 plt.plot(time,th1,color='b',lw=1,ls='-',label='theta1(t)')
 plt.ylabel('theta1',fontsize=12)
-plt.yticks(np.arange(-4,4.5,1))
+
 plt.xlabel('t[s]',fontsize=12)
-plt.xticks(np.arange(0,10.1,1))
+plt.xticks(np.arange(0,t,1))
 plt.legend(loc='upper right')
 plt.xlabel('t[t]')
 plt.ylabel('theta1')
@@ -23,7 +28,7 @@ plt.plot(time,th2,color='r',lw=1,ls='-',label='theta2(t)')
 plt.legend(loc='upper right')
 plt.xlabel('t[s]')
 plt.ylabel('theta2')
-
+plt.xticks(np.arange(0,t,1))
 
 
 plt.subplot(223)
@@ -31,13 +36,15 @@ plt.plot(time,om1,color='g',lw=1,ls='-',label='omega1(t)')
 plt.legend(loc='upper right')
 plt.xlabel('t[s]')
 plt.ylabel('omega1[rad/s]')
-
+plt.xticks(np.arange(0,t,1))
 
 plt.subplot(224)
 plt.plot(time,om2,color='y',lw=1,ls='-',label='omega2(t)')
 plt.legend(loc='upper right')
 plt.xlabel('t[s]')
 plt.ylabel('omega2[rad/s]')
+plt.xticks(np.arange(0,t,1))
+
 
 plt.show()
 
