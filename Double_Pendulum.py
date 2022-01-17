4ff9b77a23ee412d36949c45cbfad179a29650ae
BartoszMarsz <marsz.bartosz@gmail.com>
2022-01-17 23:12:57 +0100

    Graphical interface

--- Double_Pendulum.py
+++ Double_Pendulum.py
@@ -1,27 +1,87 @@
 from Data_output import *
 import numpy as np
-import time
+from tkinter import *
 
-l1 = 1
-l2 = 2
-m1 = 1
-m2 = 1
-h = 0.01
-t = 15
+root = Tk()
+root.title('Double Pendulum')
 
-const = (t, h, l1, l2, m1, m2)
+t_th1 = Label(root, text='theta1 [rad]')
+t_th2 = Label(root, text='theta2 [rad]')
+t_om1 = Label(root, text='omega1 [rad]')
+t_om2 = Label(root, text='omega2 [rad]')
 
-th1 = np.pi / 2
-th2 = 2 * np.pi / 3
-om1 = 0
-om2 = 0
+e_th1 = Entry(root, width=15, borderwidth=5)
+e_th1.insert(0,0)
+e_th2 = Entry(root, width=15, borderwidth=5)
+e_th2.insert(0,0)
+e_om1 = Entry(root, width=15, borderwidth=5)
+e_om1.insert(0,0)
+e_om2 = Entry(root, width=15, borderwidth=5)
+e_om2.insert(0,0)
 
-var = [th1, th2, om1, om2]
+t_l1 = Label(root, text='l1 [m]')
+t_l2 = Label(root, text='l2 [m]')
+t_m1 = Label(root, text='m1 [kg]')
+t_m2 = Label(root, text='m2 [kg]')
 
-t1 = time.time()
-output(const, var)
-t2 = time.time()
-print(t2 - t1)
+e_l1 = Entry(root, width=15, borderwidth=5)
+e_l1.insert(0,1)
+e_l2 = Entry(root, width=15, borderwidth=5)
+e_l2.insert(0,1)
+e_m1 = Entry(root, width=15, borderwidth=5)
+e_m1.insert(0,1)
+e_m2 = Entry(root, width=15, borderwidth=5)
+e_m2.insert(0,1)
+
+t_t = Label(root, text='How many seconds?')
+e_t = Entry(root, width=15, borderwidth=5)
+
+def Go():
+    l1 = float(e_l1.get())
+    l2 = float(e_l2.get())
+    m1 = float(e_m1.get())
+    m2 = float(e_m2.get())
+    h = 0.01
+    t = float(e_t.get())
+    const = (t, h, l1, l2, m1, m2)
+    th1 = float(e_th1.get())
+    th2 = float(e_th2.get())
+    om1 = float(e_om1.get())
+    om2 = float(e_om2.get())
+    var = [th1, th2, om1, om2]
+    output(const, var)
+    root.quit()
+
+
+b_plot = Button(root, text='Go!', command=Go, padx=50)
+
+t_th1.grid(column=0, row=0)
+t_th2.grid(column=0, row=1)
+t_om1.grid(column=0, row=2)
+t_om2.grid(column=0, row=3)
+
+e_th1.grid(column=1, row=0)
+e_th2.grid(column=1, row=1)
+e_om1.grid(column=1, row=2)
+e_om2.grid(column=1, row=3)
+
+t_l1.grid(column=2, row=0)
+t_l2.grid(column=2, row=1)
+t_m1.grid(column=2, row=2)
+t_m2.grid(column=2, row=3)
+
+e_l1.grid(column=3, row=0)
+e_l2.grid(column=3, row=1)
+e_m1.grid(column=3, row=2)
+e_m2.grid(column=3, row=3)
+
+t_t.grid(column=0, row=4)
+e_t.grid(column=1, row=4)
+
+b_plot.grid(column=0, row=5, columnspan=4)
+
+root.mainloop()
+
 from plot import *
 from animation import *
 
--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-x = [1, 2, 3]
-print(x[:2])
+from tkinter import *
+
+root = Tk()
+root.title('Double Pendulum')
+
+e = Entry(root, width='35', borderwidth=5, bg='blue', fg='white')
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+#e.insert(0, 'Enter your number: ')
+
+#myLabel1 = Label(root, text='Hello world!').grid(row=0, column=0)
+def myClick(number):
+    e.delete(0,END)
+    e.insert(0,number)
+    return
+myButton = Button(root, text='Enter your name', command=lambda: myClick('Bartek'), padx=50, pady=25, fg='#ffffff', bg='red')
+myButton.grid(row=1,column=0)
+
+root.mainloop()
